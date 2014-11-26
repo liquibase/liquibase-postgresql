e986f79b2628f0aaf61bffe10f992067316d31ad
Nathan Voxland <nathan@voxland.net>
2014-11-26 10:20:51 -0600

    Updated for Liquibase 3.3.0 support and buildserver

--- src/test/java/liquibase/ext/vacuum/VacuumChangeTest.java
+++ src/test/java/liquibase/ext/vacuum/VacuumChangeTest.java
@@ -1,36 +1,51 @@
 package liquibase.ext.vacuum;
 
+import junit.framework.Assert;
 import liquibase.Liquibase;
 import liquibase.database.DatabaseFactory;
+import liquibase.database.core.PostgresDatabase;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.statement.SqlStatement;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.Driver;
 import java.util.Properties;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class VacuumChangeTest {
-    
+
     @Test
-    public void test() throws Exception {
-        String url = "jdbc:postgresql://10.10.100.100/lbcat";
-        final Driver driver = (Driver) Class.forName(DatabaseFactory.getInstance().findDefaultDriver(url), true, Thread.currentThread().getContextClassLoader()).newInstance();
+    public void generateStatements() {
+        VacuumChange change = new VacuumChange();
+        change.setTableName("my_table");
+        SqlStatement[] sqlStatements = change.generateStatements(new PostgresDatabase());
 
-        Properties info = new Properties();
-        info.put("user", "lbuser");
-        info.put("password", "lbuser");
-
-        Connection connection = driver.connect(url, info);
-        if (connection == null) {
-            throw new DatabaseException("Connection could not be created to " + url + " with driver " + driver.getClass().getName() + ".  Possibly the wrong driver for the given database URL");
-        }
-
-        Liquibase liquiBase = new Liquibase("liquibase/ext/vacuum/changelog.test.xml", new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
-        liquiBase.dropAll();
-        
-        liquiBase = new Liquibase("liquibase/ext/vacuum/changelog.test.xml", new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
-        liquiBase.update((String) null);
+        assertEquals(1, sqlStatements.length);
+        assertEquals("my_table", ((VacuumStatement) sqlStatements[0]).getTableName());
     }
+
+//    @Test
+//    public void test() throws Exception {
+//        String url = "jdbc:postgresql://10.10.100.100/lbcat";
+//        final Driver driver = (Driver) Class.forName(DatabaseFactory.getInstance().findDefaultDriver(url), true, Thread.currentThread().getContextClassLoader()).newInstance();
+//
+//        Properties info = new Properties();
+//        info.put("user", "lbuser");
+//        info.put("password", "lbuser");
+//
+//        Connection connection = driver.connect(url, info);
+//        if (connection == null) {
+//            throw new DatabaseException("Connection could not be created to " + url + " with driver " + driver.getClass().getName() + ".  Possibly the wrong driver for the given database URL");
+//        }
+//
+//        Liquibase liquiBase = new Liquibase("liquibase/ext/vacuum/changelog.test.xml", new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
+//        liquiBase.dropAll();
+//
+//        liquiBase = new Liquibase("liquibase/ext/vacuum/changelog.test.xml", new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
+//        liquiBase.update((String) null);
+//    }
 }
