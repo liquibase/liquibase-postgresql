247ec792e79c12929e1d867f67b8d7eab18493b3
Alex Brackx <abrackx@gmail.com>
2024-11-07 09:58:15 -0700

    [fix-integration-tests-sonar] Fix integration test

--- src/test/java/liquibase/ext/copy/CopyChangeIT.java
+++ src/test/java/liquibase/ext/copy/CopyChangeIT.java
@@ -5,7 +5,10 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.sql.Connection;
 import java.sql.Driver;
@@ -13,20 +16,33 @@ import java.util.Properties;
 
 
 public class CopyChangeIT {
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @Before
+    public void setup() {
+        postgres.start();
+    }
+
+    @After
+    public void stop() {
+        postgres.stop();
+    }
 
     @Test
     public void test() throws Exception {
-        String url = "jdbc:postgresql://localhost/lbtest";
+        String url = postgres.getJdbcUrl();
         final Driver driver = (Driver) Class.forName(DatabaseFactory.getInstance().findDefaultDriver(url), true, Thread.currentThread().getContextClassLoader()).newInstance();
 
         Properties info = new Properties();
-        info.put("user", "lbtest");
-        info.put("password", "lbtest");
+        info.put("user", postgres.getUsername());
+        info.put("password", postgres.getPassword());
 
         Connection connection = driver.connect(url, info);
         if (connection == null) {
             throw new DatabaseException("Connection could not be created to " + url + " with driver " + driver.getClass().getName() + ".  Possibly the wrong driver for the given database URL");
-            
+
         }
 
         Liquibase liquiBase = new Liquibase("liquibase/ext/copy/changelog.test.xml", new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
